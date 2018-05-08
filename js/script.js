73944c779210c7b17656cc0d5127d734a9913075
mdougllas <mdougllas@gmail.com>
2018-05-08 12:10:15 -0400

    Game engine almost done. Need to create a timer

--- js/script.js
+++ js/script.js
@@ -4,47 +4,69 @@ window.onload = function(){
   canvas = document.getElementById("main-canvas");
   ctx = canvas.getContext("2d");
 
+  //Drawing the board score
+  let boardScore = 0;
+  ctx.fillRect (6, 12, 120, 20);
+
+
   //Defining ghosts images
   let defaultGhost = new Image();
   defaultGhost.src = 'images/ghost.png';
+  let ghostPos = []; //Initial ghost position
 
   //Generating random between 2 given numbers
   function randomBtw(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   }
   
-
+ //Making ghost randomly popping
   function popGhost() {
     let randomX = randomBtw(30, 920);
     let randomY = randomBtw(30, 620);
-    ctx.clearRect(0,0,950,650);
-    ctx.fillRect(randomX,randomY,35,35);
+    ctx.clearRect(ghostPos[0],ghostPos[1],35,35);
+    //ctx.fillRect(randomX,randomY,35,35);
     ctx.drawImage(defaultGhost, randomX,randomY, 35, 35 );
-    let ghostPos = [randomX, randomY];
-    return ghostPos;
+    ghostPos = [randomX, randomY];
+    return ghostPos; //Need the position to compare with click
   }
 
-
+  //Making the ghost pops up with random times
   (function loop() {
-      var rand = randomBtw(600, 1500);
-      setTimeout(function() {
-        let b = popGhost();
-        loop();
-        console.log(b);
-
-        window.onclick = function(e){
-          console.log(e.layerX);
-          console.log(e.layerY);
-        }
+    //Positioning the score
+    ctx.clearRect(6, 12, 120, 20);
+    ctx.font = '24px Arial';
+    ctx.fillText("Score: "+boardScore, 20, 30);
+    
+    //New random position each iteration
+    var time = randomBtw(300, 900);
+    setTimeout(function() {
+      let ghostPosition = popGhost(); //Getting the ghost position
+      loop();
       
-      }, rand);
+      let hit = false;
+      window.onclick = function(e){
+        let clickPos = [e.layerX, e.layerY]; //Getting the click position
+        // console.log("cli",clickPos[0]);
+        // console.log(ghostPosition[0]);
+        ctx.fillRect(clickPos[0],clickPos[1],5,5);
+
+        if(clickPos[0] > ghostPosition[0] & clickPos[0] < ghostPosition[0]+35){
+          if(clickPos[1] > ghostPosition[1] & clickPos[1] < ghostPosition[1]+35){
+            if(hit == true){
+              console.log("bluuu");
+            } else {
+              console.log("bleh");
+              hit = true;
+              boardScore ++;
+            }
+          }
+        }
+      }
+
+    }, time);
   }());
 
   
-
-
-
-
   ///REFERENCE FUNCTION
 
   // function writeMessage(canvas, message) {
