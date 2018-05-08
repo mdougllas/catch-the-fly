9a4679dc234d58d6dcddcaa74f832d44a82e73c4
mdougllas <mdougllas@gmail.com>
2018-05-08 14:54:51 -0400

    Game working. Need to make it to finish when the time ends

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 window.onload = function(){
 
-  //Defining canvas
+  //Defining canvas and styles
   canvas = document.getElementById("main-canvas");
   ctx = canvas.getContext("2d");
+  ctx.font = '24px Titan One';
 
   //Drawing the board score
   let boardScore = 0;
-  ctx.fillRect (6, 12, 120, 20);
+  //ctx.fillRect (6, 12, 150, 20);//Position reference
+  //ctx.fillRect (780, 10, 155, 20);//Reference for timer
 
+  //Defining game timing and countdown timer
+  var min = 3; //
+  var sec = 59;
+  let timer = null;
 
   //Defining ghosts images
   let defaultGhost = new Image();
@@ -22,10 +28,9 @@ window.onload = function(){
  //Making ghost randomly popping
   function popGhost() {
     let randomX = randomBtw(30, 920);
-    let randomY = randomBtw(30, 620);
-    ctx.clearRect(ghostPos[0],ghostPos[1],35,35);
-    //ctx.fillRect(randomX,randomY,35,35);
-    ctx.drawImage(defaultGhost, randomX,randomY, 35, 35 );
+    let randomY = randomBtw(30, 610);
+    ctx.clearRect(ghostPos[0],ghostPos[1],35,35); //Cler last ghost
+    ctx.drawImage(defaultGhost, randomX,randomY, 35, 35 ); //Create new ghost
     ghostPos = [randomX, randomY];
     return ghostPos; //Need the position to compare with click
   }
@@ -33,25 +38,26 @@ window.onload = function(){
   //Making the ghost pops up with random times
   (function loop() {
     //Positioning the score
-    ctx.clearRect(6, 12, 120, 20);
-    ctx.font = '24px Arial';
+    ctx.clearRect(6, 12, 150, 25);
     ctx.fillText("Score: "+boardScore, 20, 30);
     
     //New random position each iteration
-    var time = randomBtw(300, 900);
+    var time = randomBtw(300, 900);//Random time to ghost appears
     setTimeout(function() {
       let ghostPosition = popGhost(); //Getting the ghost position
       loop();
       
-      let hit = false;
+      let hit = false; //Trigger when hit ghost
       window.onclick = function(e){
         let clickPos = [e.layerX, e.layerY]; //Getting the click position
-        // console.log("cli",clickPos[0]);
-        // console.log(ghostPosition[0]);
-        ctx.fillRect(clickPos[0],clickPos[1],5,5);
 
+        ctx.fillRect(clickPos[0],clickPos[1],5,5); //Pointing clicked spots
+
+        //Checking when player hits ghost and increasing score
         if(clickPos[0] > ghostPosition[0] & clickPos[0] < ghostPosition[0]+35){
           if(clickPos[1] > ghostPosition[1] & clickPos[1] < ghostPosition[1]+35){
+
+            //Preventing the player to hit the same target more than once
             if(hit == true){
               console.log("bluuu");
             } else {
@@ -59,15 +65,39 @@ window.onload = function(){
               hit = true;
               boardScore ++;
             }
+
           }
-        }
+        }//I will put an else here with the 'miss' sound
+        
       }
 
     }, time);
   }());
 
-  
-  ///REFERENCE FUNCTION
+
+  //Simple timer
+  function timing(){
+    ctx.clearRect(780, 10, 155, 20);
+    ctx.fillText("Time: " + min + ":" + sec, 800, 30);
+    sec--;
+    if(sec < 10){
+      sec = "0" + sec;
+    }
+    if(sec == "0-1"){
+      min--;
+      sec = 60;
+    }
+    if(min < 0 && sec == 60){
+      clearInterval(timer);
+    }
+  }
+  timer = setInterval(timing, 1000);
+
+
+}
+
+
+  ///REFERENCE FUNCTION - SHOWING MOUSE POSITION SCREEN
 
   // function writeMessage(canvas, message) {
   //   ctx.clearRect(0, 0, 500, 120);
@@ -88,5 +118,3 @@ window.onload = function(){
   //   var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
   //   writeMessage(canvas, message);
   // }, false);
-
-}
